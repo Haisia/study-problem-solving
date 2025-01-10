90073445f67a2705ac68f49e8d858a19541eefb8
최준혁 <cwnsgur13579@gmail.com>
2025-01-10 17:30:55 +0900

    boj: p10989

---
+++ boj/src/main/java/org/haisia/p10989/Main.java
@@ -0,0 +1,25 @@
+package org.haisia.p10989;
+
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    int totalCount = Integer.parseInt(br.readLine());
+
+    int[] cache = new int[10001];
+    for (int i = 0; i < totalCount; i++) {
+      cache[Integer.parseInt(br.readLine())]++;
+    }
+
+    for (int i = 1; i < cache.length; i++) {
+      for (int j = 0; j < cache[i]; j++) {
+        sb.append(i).append('\n');
+      }
+    }
+
+    System.out.println(sb);
+  }
+}
