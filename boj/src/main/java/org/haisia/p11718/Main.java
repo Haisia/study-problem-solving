e2474919cf45929a7beb65320aab773311b970bc
최준혁 <cwnsgur13579@gmail.com>
2025-01-10 16:16:56 +0900

    boj: p11718

---
+++ boj/src/main/java/org/haisia/p11718/Main.java
@@ -0,0 +1,12 @@
+package org.haisia.p11718;
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    while (sc.hasNextLine()) {
+      System.out.println(sc.nextLine());
+    }
+  }
+}
